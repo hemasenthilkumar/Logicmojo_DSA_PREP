6562b0e9ac00fb3e5f7e2eb7ecbdbcb740f77018
hemapriya <hema.senthilkumar@outlook.com>
2025-08-07 16:24:16 +0530

    Adding session 2

---
+++ jul_27_s2/prime_number.py
@@ -0,0 +1,15 @@
+"""
+1. Check if a given number is prime number
+
+Approach : Iterate till square root of the number and check if its divisible O(sqrt(n))
+
+2. Get all prime numbers within given number
+
+Approach 1: Naive Approach
+
+For each number between the range, check if the number is prime number O(n.sqrt(n))
+
+Approach 2: Sieve of Eratosthenes
+
+For each number, we strike off the further multiples untill the range
+"""
