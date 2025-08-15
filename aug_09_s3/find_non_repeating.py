2856dfde83017395af1f0685b57f2a7e91d11a15
hemapriya <hema.senthilkumar@outlook.com>
2025-08-15 20:52:43 +0530

    Adding another approach

--- aug_09_s3/find_non_repeating.py
+++ aug_09_s3/find_non_repeating.py
@@ -7,9 +7,26 @@ Output: [7, 9]
 Explanation: 7 and 9 appear only once; all others appear twice.
 """
 
+
+def find_non_repeat_a2(x):
+    # TC: O(n)
+    # SC: O(n)
+    freq_dict = {}
+    for i in x: # O(n)
+        if i in freq_dict:
+            freq_dict[i] += 1
+        else:
+            freq_dict[i] = 1 
+    res = []
+    for k,v in freq_dict.items():  # O(n)
+        if v == 1:
+            res.append(k)
+    return res
+
 def find_non_repeat(x):
     # TC: O(nlogn)
     # Sort: O(nlogn) + Traverse: O(n) 
+    # SC: O(1)
     res = []
     i = 0
     x.sort()
@@ -24,4 +41,11 @@ def find_non_repeat(x):
 
 if __name__ == "__main__":
     x= [-1, -2, -1, -3, -2, -4]
+    y =[10,20]
+    z=[10]
     print(find_non_repeat(x))
+    print(find_non_repeat_a2(x))
+    print(find_non_repeat(y))
+    print(find_non_repeat_a2(y))
+    print(find_non_repeat(z))
+    print(find_non_repeat_a2(z))
