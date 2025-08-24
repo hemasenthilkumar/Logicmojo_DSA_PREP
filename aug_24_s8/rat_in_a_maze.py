2fde8e3272a02d6c8f0e5a9b05bb04fd24c34020
hemapriya <hema.senthilkumar@outlook.com>
2025-08-24 12:46:27 +0530

    Adding rat in maze

--- aug_24_s8/rat_in_a_maze.py
+++ aug_24_s8/rat_in_a_maze.py
@@ -16,3 +16,60 @@ Safe method:
 - cell value is 1
 - tracking visited matrix
 """
+
+rowMoves = [-1,1,0,0]
+colMoves = [0,0,-1,1]
+direction = ['U', 'D', 'L', 'R']
+
+def isSafe(row, col, n, board, visited):
+    if row >= 0 and col >=0 and row < n and col < n and (row, col) not in visited and board[row][col] == 1:
+        return True 
+    return False
+
+def traverse(row, col, board, n, visited, path, result):
+
+    if row == n-1 and col == n-1:
+        result.append("".join(path))
+        return 
+    
+    nextRow, nextCol = 0,0
+    for i in range(4):
+        nextRow = row + rowMoves[i]
+        nextCol = col + colMoves[i]
+        if isSafe(nextRow, nextCol, n, board, visited):
+            visited.add((nextRow, nextCol))
+            path.append(direction[i])
+            
+            traverse(nextRow, nextCol, board, n, visited, path, result)
+
+            path.pop()
+            visited.remove((nextRow, nextCol))
+
+def solveRatMaze(board, n):
+    visited = set()
+    result = []
+    visited.add((0,0))
+    traverse(0,0, board, n,  visited, [], result)
+    if bool(result):
+        print(f"Reached: {result}")
+    else:
+        print("No solution!")
+
+
+
+if __name__ == "__main__":
+    n = 3
+    maze = [
+      [1, 0, 0],
+      [0, 1, 0],
+      [0, 0, 1]
+    ]
+    solveRatMaze(maze, n)
+    n = 4
+    maze = [
+      [1, 1, 0, 0],
+      [1, 1, 0, 1],
+      [0, 1, 1, 1],
+      [1, 1, 1, 1]
+    ]
+    solveRatMaze(maze, n)
