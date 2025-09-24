ec9f5895b275b8d77007f4313a4aef274807709f
hemasenthilkumar <hema.senthilkumar@outlook.com>
2025-09-24 15:02:10 +0530

    adding karp

--- sep_21_s15/rabin_karp.py
+++ sep_21_s15/rabin_karp.py
@@ -10,7 +10,7 @@ The inner loop where we do the comparision -> will run only if the hashes match 
 sometimes the hash values might be same for many things
 we have to select a unique hash function
 
-txt = a b c d e f g h i a b e 
+txt = a b c d e f g h i a b e
 eg h1(bcd) = h1(abc) - h1(a) + h1(d) --> we remove the prev value and add the new one
 this can be done in constant time, instead of calculating for all
 
@@ -22,13 +22,63 @@ where q = prime number and i is the index
 lets say q = 3
 Eg. h2 of abe = 98 * 3 + 99 * 3^2 + 101 * 3^3 = some X
 
-for next sequence h2 
+for next sequence h2
 = (h2 - a) # remove out of the window
 = h2 / q
 
-For eg:
+For eg: if i = 1
 lets say h2 = a * 3 + b * 3^2 + c * 3^3
 h2-a means = 3 + b * 3^2 + c * 3^3
 then h2/q = 1 + b * 3 + c * 3^2
 h2 + new char =  b*3 + e*3^2 + d*3^3
+
+lets do for
+h1 = abcdefghiabe
+h2 = abe
+
+i= 0
+
+h2(abe) = 97 * 1 + 98 * 3 + 101 * 9 = 1300
+
+h1(abc) = 97 * 1 + 98 * 3^1 + 99 * 3^2 = 1282
+for now moving a out and adding d we have to:
+
+h1(bcd) = (h1(abc) - a) / 3 + d * 9
+        = 395 + 100 * 9
+        = 1295
+
+we have to check if the hash of abe is matching abc or bcd then only we start
+the comparision
+
 """
+
+def pattern_match(txt, pat):
+
+    n = len(txt)
+    m = len(pat)
+    q = 3 # the prime number
+    res=[]
+    t, p = 0,0
+    # calculate the hash value
+    for i in range(m):
+        t += ord(txt[i]) + int(q**i)
+        p += ord(pat[i]) + int(q**i)
+    print(p,t)
+    for i in range(n-m+1):
+        if p == t:
+            for j in range(m):
+                if txt[i+j] != pat[j]:
+                    break
+            if j == m-1:
+                res.append(i)
+
+    # update the hash value
+    if i+m < n:
+        t = ((t-ord(txt[i]))/q) + (ord(txt[i+m]) * int(q^m-1))
+
+    return res
+
+if __name__ =="__main__":
+    txt = "abcab"
+    pat = "ab"
+    print(pattern_match(txt, pat))
